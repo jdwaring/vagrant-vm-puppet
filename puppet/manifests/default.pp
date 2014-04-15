5116700b751f529c54ca5692a561178e0c12222d
John Waring <john@concept2020.net>
2014-04-15 18:38:02 +0100

    Adding modules for php

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,4 +1,31 @@
+# create a new run stage to ensure certain modules are included first
+
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+stage { 'pre':
+  before => Stage['main']
+}
+
+# add the baseconfig module to the new 'pre' run stage
+class { 'baseconfig':
+  stage => 'pre'
+}
+
+# set defaults for file ownership/permissions
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+}
+
+include baseconfig
 include apache
-include system-update
+include mysql
+include php
+
+
+
+
+
+
+
--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -1,13 +1,31 @@
+# == Class: apache
+#
+# Installs packages for Apache2, enables modules, and sets config files.
+#
 class apache {
 
-  package { "apache2":
-    ensure  => present,
-    require => Class["system-update"],
+  package { ['apache2', 'apache2-mpm-prefork']:
+    ensure => present;
   }
 
-  service { "apache2":
-    ensure  => "running",
-    require => Package["apache2"],
+  service { 'apache2':
+    ensure  => running,
+    require => Package['apache2'];
   }
+
+  apache::conf { [
+    'apache2.conf', 
+    'envvars', 
+    'ports.conf'
+  ]: }
+
+
+  apache::module { [
+    'expires.load', 
+    'proxy.conf', 
+    'proxy.load', 
+    'proxy_http.load', 
+    'rewrite.load'
+  ]: }
 
 }
